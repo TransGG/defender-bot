7891430865cb8e6dcb3b3573ddd298a42ad664f6
slvr.dev <lily@silverlily.net>
2022-04-03 16:22:02 -0600

    fix: asdf

--- src/oauth/oathServer.ts
+++ src/oauth/oathServer.ts
@@ -1,18 +1,24 @@
 import express from "express";
-import DiscordOath2 from "discord-oauth2";
+import url from "url";
+import fetch from "node-fetch";
 
 import { validateConfig, Config } from "../utils/validateConfig.js";
 
 let config: Config = validateConfig();
 
 const app = express();
-const oauth = new DiscordOath2({
-  clientId: config.tokens.discordAppId,
-  clientSecret: config.tokens.discordAppSecret,
-  redirectUri: config.oath.url,
-});
+
+console.log("redr uri: " + config.oath.url);
 
 let ipScores: { [key: string]: number } = {};
+
+interface oauthData {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+}
 
 const ipScoreTimer = 10; // in minutes
 setInterval(() => {
@@ -25,46 +31,87 @@ setInterval(() => {
   }
 }, ipScoreTimer * 60 * 1000);
 
-const scopes = "";
+app.get("/oath2", async (req, response) => {
+  try {
+    let ip = req.ip;
+    if (ipScores[ip]) {
+      if (ipScores[ip]! > 6) {
+        // drop connection
+        response.statusCode = 429;
+        response.send("Request ratelimited.");
+        return;
+      }
+    } else {
+      ipScores[ip] = 0;
+    }
 
-app.get("/", async (req, response) => {
-  let ip = req.ip;
-  if (ipScores[ip]) {
-    if (ipScores[ip]! > 10) {
-      // drop connection
-      response.statusCode = 429;
-      response.send("Request ratelimited.");
+    ipScores[ip]++;
+
+    console.log("got oath request from: " + ip + " score " + ipScores[ip]);
+
+    let code = req.query["code"];
+
+    if (typeof code != "string") {
+      ipScores[ip] += 4;
       return;
     }
-  } else {
-    ipScores[ip] = 0;
-  }
 
-  ipScores[ip]++;
+    console.log(`got oath2 code: ${code}`);
 
-  let code = req.query["code"];
-
-  if (!code || typeof code != "string") {
-    ipScores[ip] += 3;
-    return;
-  }
-
-  if (code) {
     try {
-      let dat = await oauth.tokenRequest({
-        code: code,
-        scope: scopes,
-        grantType: "authorization_code",
+      let oathResult = await fetch("https://discord.com/api/v9/oauth2/token", {
+        method: "POST",
+        body: new url.URLSearchParams({
+          client_id: config.tokens.discordAppId,
+          client_secret: config.tokens.discordAppSecret,
+          code: code,
+          grant_type: "authorization_code",
+          redirect_uri: config.oath.url,
+          scope: "identify connections",
+        }),
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
       });
 
-      console.log(oauthData);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+      if (oathResult.status != 200) {
+        ipScores[ip] += 4;
+        response.statusCode = 403;
+        response.send("Please try again in a few minutes");
+        return;
+      }
 
-  return response.sendFile("index.html", {
-    root: ".",
-  });
+      var oauthData: oauthData = await oathResult.json();
+
+      console.log(`got user token ${JSON.stringify(oauthData)}`);
+    } catch (error) {
+      console.error("oath2 error while requesting authtoken: " + error);
+      ipScores[ip] += 4;
+      response.statusCode = 403;
+      response.send("Please try again in a few minutes");
+      return;
+    }
+
+    try {
+      const userResult = await fetch("https://discord.com/api/users/@me", {
+        headers: {
+          authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+        },
+      });
+
+      console.log(`got user ${JSON.stringify(await userResult.json(), null, 2)}`);
+    } catch (error) {
+      console.error("oath2 error while requesting user connections: " + error);
+      ipScores[ip] += 3;
+
+      response.statusCode = 403;
+      response.send("Please try again in a few minutes");
+      return;
+    }
+
+    response.statusCode = 200;
+
+    response.send("You may now close this window.");
+  } catch (error) {
+    console.error(`oath2 cought error: \n${error}`);
+  }
 });
 app.listen(config.oath.port, () => console.log(`Oath2 server listening on port ${config.oath.port}.`));
